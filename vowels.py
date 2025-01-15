2ba5937c5dc5afd2b6dcee88e7b815bff8489701
mete-rushi <meterushi021@gmail.com>
2025-01-15 10:01:04 +0530

    Made_changes

---
+++ vowels.py
@@ -0,0 +1,13 @@
+
+My_string=input("enter a name:")
+
+vowel=['a','e','i','o','u']
+
+count=0
+for char in My_string:
+    if vowel==My_string:
+        count+=1
+    else:
+        pass
+
+print(count)
